2c2529c051b64ed7ea3da3dcad4f102406f60e36
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-10-20 13:50:29 +0200

    refactor: cleanup old deprecated notifications

--- lua/lvim/config/init.lua
+++ lua/lvim/config/init.lua
@@ -7,12 +7,12 @@ local user_config_file = utils.join_paths(user_config_dir, "config.lua")
 
 ---Get the full path to the user configuration file
 ---@return string
-function M:get_user_config_path()
+function M.get_user_config_path()
   return user_config_file
 end
 
 --- Initialize lvim default configuration and variables
-function M:init()
+function M.init()
   lvim = vim.deepcopy(require "lvim.config.defaults")
 
   require("lvim.keymappings").load_defaults()
@@ -65,14 +65,6 @@ local function handle_deprecated_settings()
     end)
   end
 
-  ---lvim.lang.FOO.lsp
-  for lang, entry in pairs(lvim.lang) do
-    local deprecated_config = entry.formatters or entry.linters or {}
-    if not vim.tbl_isempty(deprecated_config) then
-      deprecation_notice(string.format("lvim.lang.%s", lang))
-    end
-  end
-
   -- lvim.lsp.override
   if lvim.lsp.override and not vim.tbl_isempty(lvim.lsp.override) then
     deprecation_notice("lvim.lsp.override", "Use `lvim.lsp.automatic_configuration.skipped_servers` instead")
@@ -81,16 +73,6 @@ local function handle_deprecated_settings()
         table.insert(lvim.lsp.automatic_configuration.skipped_servers, c)
       end
     end, lvim.lsp.override)
-  end
-
-  -- lvim.lsp.popup_border
-  if vim.tbl_contains(vim.tbl_keys(lvim.lsp), "popup_border") then
-    deprecation_notice "lvim.lsp.popup_border"
-  end
-
-  -- dashboard.nvim
-  if lvim.builtin.dashboard.active then
-    deprecation_notice("lvim.builtin.dashboard", "Use `lvim.builtin.alpha` instead. See LunarVim#1906")
   end
 
   if lvim.autocommands.custom_groups then
@@ -110,9 +92,9 @@ end
 
 --- Override the configuration with a user provided one
 -- @param config_path The path to the configuration overrides
-function M:load(config_path)
+function M.load(config_path)
   local autocmds = reload "lvim.core.autocmds"
-  config_path = config_path or self:get_user_config_path()
+  config_path = config_path or M.get_user_config_path()
   local ok, err = pcall(dofile, config_path)
   if not ok then
     if utils.is_file(user_config_file) then
@@ -147,7 +129,7 @@ end
 
 --- Override the configuration with a user provided one
 -- @param config_path The path to the configuration overrides
-function M:reload()
+function M.reload()
   vim.schedule(function()
     reload("lvim.utils.hooks").run_pre_reload()
 
